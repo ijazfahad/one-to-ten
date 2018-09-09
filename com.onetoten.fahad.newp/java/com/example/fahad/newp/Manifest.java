0fd71906a0432ae1e417920ca7425435083b5ff8
Fahad <ijazfahad@yahoo.com>
2018-09-08 20:46:38 -0500

    one to ten complete

--- com.onetoten.fahad.newp/java/com/example/fahad/newp/Manifest.java
+++
@@ -1,8 +0,0 @@
-package com.example.fahad.newp;
-
-public final class Manifest {
-
-    public static final class permission {
-        public static final String C2D_MESSAGE = "com.example.fahad.newp.permission.C2D_MESSAGE";
-    }
-}
